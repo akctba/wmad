a4edc4e1df7c692465c196c37dde5a25f13ac0fc
Alex Kayser <akctba@gmail.com>
2019-11-10 19:55:46 -0800

    random test

--- src/random/RandomEval.java
+++ src/random/RandomEval.java
@@ -1,25 +1,48 @@
 package random;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.GridLayout;
-import javax.swing.JLabel;
-import javax.swing.JSpinner;
-import javax.swing.JToggleButton;
+import java.awt.event.ActionEvent;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
-import javax.swing.DefaultComboBoxModel;
-
-
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 public class RandomEval {
 
 	private JFrame frmRandomEvaluation;
+
+	// fields
+	private JComboBox<Methods> comboMethod;
+	private JSpinner spnQuantity;
+	private JSpinner spnFrom;
+	private JSpinner spnTo;
+	private JComboBox<Duplicate> comboDuplicate;
+	private JList<String> list;
+	private JLabel lblDuplicates;
+	private JLabel lblPercduplicates;
 	private JTable tableReincidence;
+
+	private final Action runButton = new RunAction();
 
 	/**
 	 * Launch the application.
@@ -50,73 +73,199 @@ public class RandomEval {
 	private void initialize() {
 		frmRandomEvaluation = new JFrame();
 		frmRandomEvaluation.setTitle("Random Evaluation");
-		frmRandomEvaluation.setBounds(100, 100, 450, 400);
+		frmRandomEvaluation.setBounds(100, 100, 450, 413);
 		frmRandomEvaluation.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRandomEvaluation.getContentPane().setLayout(null);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBounds(6, 6, 438, 121);
+		panel.setBounds(6, 6, 438, 139);
 		frmRandomEvaluation.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(0, 2, 0, 0));
-		
+
+		JLabel lblMethod = new JLabel("Method");
+		panel.add(lblMethod);
+
+		comboMethod = new JComboBox<Methods>();
+		comboMethod.setName("comboMethod");
+		comboMethod.setModel(new DefaultComboBoxModel<Methods>(random.Methods.values()));
+		panel.add(comboMethod);
+
 		JLabel lblQuantity = new JLabel("Quantity");
 		panel.add(lblQuantity);
-		
-		JSpinner SpnQuantity = new JSpinner();
-		panel.add(SpnQuantity);
-		
+
+		spnQuantity = new JSpinner();
+		panel.add(spnQuantity);
+		spnQuantity.setValue(100);
+
 		JLabel lblFrom = new JLabel("From");
 		panel.add(lblFrom);
-		
-		JSpinner SpnFrom = new JSpinner();
-		panel.add(SpnFrom);
-		
-		JLabel lblTo = new JLabel("To:");
+
+		spnFrom = new JSpinner();
+		panel.add(spnFrom);
+
+		JLabel lblTo = new JLabel("To");
 		panel.add(lblTo);
-		
-		JSpinner spnTo = new JSpinner();
+
+		spnTo = new JSpinner();
+		spnTo.setValue(1000);
 		panel.add(spnTo);
-		
+
 		JLabel lblDuplicatesAreAllowed = new JLabel("Duplicates are allowed");
 		panel.add(lblDuplicatesAreAllowed);
-		
-		JComboBox comboDuplicate = new JComboBox();
-		comboDuplicate.setModel(new DefaultComboBoxModel(random.duplicate.values()));
+
+		comboDuplicate = new JComboBox<Duplicate>();
+		comboDuplicate.setName("comboDuplicate");
+		comboDuplicate.setModel(new DefaultComboBoxModel<Duplicate>(random.Duplicate.values()));
 		panel.add(comboDuplicate);
-		
+
 		JLabel lblGeneratedNumbers = new JLabel("Generated numbers");
-		lblGeneratedNumbers.setBounds(6, 136, 138, 16);
+		lblGeneratedNumbers.setBounds(6, 157, 138, 16);
 		frmRandomEvaluation.getContentPane().add(lblGeneratedNumbers);
-		
-		JList list = new JList();
+
+		list = new JList<String>();
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setBounds(6, 164, 172, 196);
-		frmRandomEvaluation.getContentPane().add(list);
-		
+		JScrollPane scrollPane = new JScrollPane(list);
+		scrollPane.setBounds(6, 185, 172, 196);
+		frmRandomEvaluation.add(scrollPane);
+
 		JLabel lblStatistics = new JLabel("Statistics");
-		lblStatistics.setBounds(190, 139, 61, 16);
+		lblStatistics.setBounds(190, 157, 61, 16);
 		frmRandomEvaluation.getContentPane().add(lblStatistics);
-		
+
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(190, 164, 254, 33);
+		panel_1.setBounds(190, 186, 254, 33);
 		frmRandomEvaluation.getContentPane().add(panel_1);
 		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		JLabel lblDup = new JLabel("Duplicates");
 		panel_1.add(lblDup);
-		
-		JLabel lblDuplicates = new JLabel("duplicates");
+
+		lblDuplicates = new JLabel("duplicates");
+		lblDuplicates.setText("-");
 		panel_1.add(lblDuplicates);
-		
-		JLabel lblPercduplicates = new JLabel("percDuplicates");
+
+		lblPercduplicates = new JLabel("percDuplicates");
+		lblPercduplicates.setText("-%");
 		panel_1.add(lblPercduplicates);
-		
+
 		tableReincidence = new JTable();
-		tableReincidence.setBounds(190, 210, 254, 150);
-		frmRandomEvaluation.getContentPane().add(tableReincidence);
+		JScrollPane scrollTable = new JScrollPane(tableReincidence);
+		scrollTable.setBounds(190, 231, 254, 150);
+		frmRandomEvaluation.getContentPane().add(scrollTable);
+
+		JButton btnRun = new JButton("Run");
+		btnRun.setAction(runButton);
+		btnRun.setBounds(327, 152, 117, 29);
+		frmRandomEvaluation.getContentPane().add(btnRun);
+	}
+
+	private class RunAction extends AbstractAction {
+		public RunAction() {
+			putValue(NAME, "Run");
+			putValue(SHORT_DESCRIPTION, "Verify random eficiency");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+
+			// get the values
+			Methods method = (Methods) comboMethod.getSelectedItem();
+//			private JSpinner spnQuantity;
+//			private JSpinner spnFrom;
+//			private JSpinner spnTo;
+//			private JComboBox<Duplicate> comboDuplicate;
+			Duplicate dup = (Duplicate) comboDuplicate.getSelectedItem();
+//			private JList<String> list;
+//			private JLabel lblDuplicates;
+//			private JLabel lblPercduplicates;
+//			private JTable tableReincidence;
+			int from = (int) spnFrom.getValue();
+			int to = (int) spnTo.getValue();
+			int quant = (int) spnQuantity.getValue();
+			int repetitions = 0;
+			DefaultListModel<String> results = new DefaultListModel<String>();
+			list.setModel(results);
+
+			Map<String, Integer> map = new HashMap<String, Integer>();
+
+			// apply to the selected method
+			switch (method) {
+			case FIRST_VERSION:
+				Random random = new Random();
+
+				for (int i = 0; i < quant; ++i) {
+					IntStream ints = random.ints(from, (to + 1));
+					OptionalInt findFirst = ints.findFirst();
+					String rdm = String.valueOf(findFirst.getAsInt());
+
+					if (results.contains(rdm)) {
+						if (dup == Duplicate.No) {
+							i--;
+						} else {
+							// Yes
+							results.addElement(rdm);
+						}
+						++repetitions;
+						// add on the table
+						if (map.containsKey(rdm)) {
+							Integer val = map.get(rdm);
+							++val;
+							map.put(rdm, val);
+						} else {
+							map.put(rdm, 1);
+						}
+
+					} else {
+						results.addElement(rdm);
+					}
+
+				}
+
+				break;
+			case SECOND_VERSION:
+
+				break;
+			default:
+				System.out.println("Please select one method.");
+			}
+
+			// shows the number of coincidences
+			lblDuplicates.setText(repetitions + "");
+			if (repetitions > 0) {
+				float perc = ((float) repetitions / quant);
+				DecimalFormat df = new DecimalFormat("0.00%");
+				lblPercduplicates.setText(df.format(perc));
+			} else {
+				lblPercduplicates.setText("0%");
+			}
+
+			tableReincidence.setModel(toTableModel(map));
+
+		}
+
+		private TableModel toTableModel(Map<String, Integer> map) {
+			DefaultTableModel model = new DefaultTableModel(new Object[] { "Key", "Value" }, 0);
+			for (Map.Entry<?, ?> entry : map.entrySet()) {
+				model.addRow(new Object[] { entry.getKey(), entry.getValue() });
+			}
+			return model;
+		}
 	}
 }
 
-enum duplicate {
+enum Duplicate {
 	Yes, No;
+}
+
+enum Methods {
+	FIRST_VERSION("First version"), SECOND_VERSION("Second version");
+
+	private String name;
+
+	private Methods(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
