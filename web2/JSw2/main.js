4039c56980d69a253798a285ae5e045ae39af5cf
Alex Kayser <akctba@gmail.com>
2020-01-31 18:32:11 -0800

    Read only to prevent double enter

--- web2/JSw2/main.js
+++ web2/JSw2/main.js
@@ -3,12 +3,14 @@
 */
 
 var btnMessage = document.getElementById('sendMessage');
+let input = document.getElementById('message');
+let output = document.getElementById('messageOutput');
 var timeout = 1500;
 var clean;
 
 btnMessage.onclick = function () {
-    let input = document.getElementById('message');
-    let output = document.getElementById('messageOutput');
+    if(input.readOnly)
+        return;
 
     if (input.value == '') {
         output.innerHTML = 'Opss..';
@@ -21,16 +23,27 @@ btnMessage.onclick = function () {
         }, timeout);
     } else {
         output.innerHTML = input.value;
-        clean = setInterval(cleanInput, timeout/20);
+        input.readOnly = true;
+        clean = setInterval(cleanInput, timeout / 20);
     }
 }
 
+input.addEventListener("keyup", () => {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        btnMessage.click();
+    }
+});
+
 function cleanInput() {
-    let input = document.getElementById('message');
     let txt = input.value;
     if (txt.length > 0) {
-        input.value = txt.substring(0, txt.length-1);
+        input.value = txt.substring(0, txt.length - 1);
     } else {
         clearInterval(clean);
+        input.readOnly = false;
     }
 }
